be6aaf42deb38dd33650f31956533f1f8926941c
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-02-02 06:41:43 -0800

    shuffling
    shuffling both images and mask_images in same order

--- __init__.py
+++ __init__.py
@@ -9,3 +9,27 @@ for x, y in numpy.ndindex(mask.shape[0], mask.shape[1]):
 
 
 print(numpy.sum(mask))
+
+
+
+import os
+from os.path import join
+from random import shuffle
+
+path_real_image = 'G:\\5 th semester\\dbms2\\image'
+real_image = os.listdir(path_real_image)
+print(type(real_image))
+
+
+path_mask_image = 'G:\\5 th semester\\dbms2\\mask'
+mask_image = os.listdir(path_mask_image)
+c = list(zip(real_image, mask_image))
+
+
+
+shuffle(c)
+
+a, b = zip(*c)
+
+for x, y in zip(a,b):
+    print(x, y)
